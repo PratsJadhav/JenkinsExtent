491df7a35d711a76177feb606e0d60bc2a190731
pj <jadhav.pratiksha001@gmail.com>
2018-07-20 10:43:44 +0530

    try 4

--- src/test/java/com/auto/PracticeC/Base.java
+++ src/test/java/com/auto/PracticeC/Base.java
@@ -25,7 +25,7 @@ public class Base {
 	{
 		Calendar calendar = Calendar.getInstance();
 	    formater	= new SimpleDateFormat("dd_MM_yyyy_hh_mm_ss");
-		extent = new ExtentReports(System.getProperty("user.dir")+"\\Reports\\test_"+formater.format(calendar.getTime()) + ".html", false);
+		extent = new ExtentReports(System.getProperty("user.dir")+"\\Reports\\test"+".html", false);
 	}
 
 	public void getResult(ITestResult result)
